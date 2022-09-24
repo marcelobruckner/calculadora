49d4f4116aaac868ee8a28761ff16b82d31f1834
Marcelo Bruckner <marcelobruckner@gmail.com>
2022-09-24 14:56:02 -0300

    ajustando referencia duplicada no pom.xml e formatando texto no tratamento de exceptions de api

--- src/main/java/br/com/calculadora/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/br/com/calculadora/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,9 +1,7 @@
 package br.com.calculadora.api.exceptionhandler;
 
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.stream.Collectors;
-
+import br.com.calculadora.exception.DivisaoPorZeroException;
+import br.com.calculadora.exception.OperandoNaoInformadoException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -17,100 +15,102 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import br.com.calculadora.exception.DivisaoPorZeroException;
-import br.com.calculadora.exception.OperandoNaoInformadoException;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
-  private static final String MSG_GENERICA_USUARIO_FINAL = "Ocorreu um erro interno inesperado no sistema. "
-      + "Tente novamente e se o problema persistir, entre em contato "
-      + "com o administrador do sistema.";
+    private static final String MSG_GENERICA_USUARIO_FINAL = new StringBuilder()
+            .append("Ocorreu um erro interno inesperado no sistema. ")
+            .append("Tente novamente e se o problema persistir, entre em contato ")
+            .append("com o administrador do sistema.").toString();
 
-  @Autowired
-  private MessageSource messageSource;
+    @Autowired
+    private MessageSource messageSource;
 
-  @ExceptionHandler(OperandoNaoInformadoException.class)
-  public ResponseEntity<?> handleOperandoNaoInformado(OperandoNaoInformadoException ex, WebRequest request) {
-    HttpStatus status = HttpStatus.BAD_REQUEST;
-    ProblemType problemType = ProblemType.RECURSO_NAO_ENCONTRADO;
-    String detail = ex.getMessage();
+    @ExceptionHandler(OperandoNaoInformadoException.class)
+    public ResponseEntity<?> handleOperandoNaoInformado(OperandoNaoInformadoException ex, WebRequest request) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        ProblemType problemType = ProblemType.RECURSO_NAO_ENCONTRADO;
+        String detail = ex.getMessage();
 
-    Problem problem = createProblemBuilder(status, problemType, detail).userMessage(MSG_GENERICA_USUARIO_FINAL).build();
+        Problem problem = createProblemBuilder(status, problemType, detail).userMessage(MSG_GENERICA_USUARIO_FINAL).build();
 
-    return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
-  }
-
-  @ExceptionHandler(DivisaoPorZeroException.class)
-  public ResponseEntity<?> handleDivisaoPorZero(DivisaoPorZeroException ex, WebRequest request) {
-    HttpStatus status = HttpStatus.BAD_REQUEST;
-    ProblemType problemType = ProblemType.RECURSO_NAO_ENCONTRADO;
-    String detail = ex.getMessage();
-
-    Problem problem = createProblemBuilder(status, problemType, detail).userMessage(MSG_GENERICA_USUARIO_FINAL).build();
-
-    return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
-  }
-
-  @Override
-  protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers,
-      HttpStatus status, WebRequest request) {
-
-    ProblemType problemType = ProblemType.RECURSO_NAO_ENCONTRADO;
-    String detail = "Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente";
-
-    BindingResult bindingResult = ex.getBindingResult();
-
-    List<Problem.Field> problemFields = bindingResult.getFieldErrors().stream()
-        .map(fieldError -> {
-
-          String message = messageSource.getMessage(fieldError, LocaleContextHolder.getLocale());
-
-          return Problem.Field.builder()
-              .name(fieldError.getField())
-              .userMessage(message)
-              .build();
-        }).collect(Collectors.toList());
-
-    Problem problem = createProblemBuilder(status, problemType, detail)
-        .userMessage(detail)
-        .fields(problemFields)
-        .build();
-
-    return handleExceptionInternal(ex, problem, headers, status, request);
-  }
-
-  @Override
-  protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
-      HttpStatus status, WebRequest request) {
-
-    if (body == null) {
-      // cairá aqui caso seja alguma exception do spring tratada por
-      // ResponseEntityExceptionHandler (classe extendida)
-      body = Problem.builder()
-          .title(status.getReasonPhrase())
-          .status(status.value())
-          .timestamp(LocalDateTime.now())
-          .userMessage(MSG_GENERICA_USUARIO_FINAL)
-          .build();
-    } else if (body instanceof String) {
-      body = Problem.builder()
-          .title((String) body)
-          .status(status.value())
-          .timestamp(LocalDateTime.now())
-          .userMessage(MSG_GENERICA_USUARIO_FINAL)
-          .build();
+        return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
     }
-    return super.handleExceptionInternal(ex, body, headers, status, request);
-  }
 
-  private Problem.ProblemBuilder createProblemBuilder(HttpStatus status, ProblemType problemType, String detail) {
-    return Problem.builder()
-        .status(status.value())
-        .type(problemType.getUri())
-        .title(problemType.getTitle())
-        .detail(detail)
-        .timestamp(LocalDateTime.now());
-  }
+    @ExceptionHandler(DivisaoPorZeroException.class)
+    public ResponseEntity<?> handleDivisaoPorZero(DivisaoPorZeroException ex, WebRequest request) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        ProblemType problemType = ProblemType.RECURSO_NAO_ENCONTRADO;
+        String detail = ex.getMessage();
+
+        Problem problem = createProblemBuilder(status, problemType, detail).userMessage(MSG_GENERICA_USUARIO_FINAL).build();
+
+        return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers,
+                                                                  HttpStatus status, WebRequest request) {
+
+        ProblemType problemType = ProblemType.RECURSO_NAO_ENCONTRADO;
+        String detail = "Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente";
+
+        BindingResult bindingResult = ex.getBindingResult();
+
+        List<Problem.Field> problemFields = bindingResult.getFieldErrors().stream()
+                .map(fieldError -> {
+
+                    String message = messageSource.getMessage(fieldError, LocaleContextHolder.getLocale());
+
+                    return Problem.Field.builder()
+                            .name(fieldError.getField())
+                            .userMessage(message)
+                            .build();
+                }).collect(Collectors.toList());
+
+        Problem problem = createProblemBuilder(status, problemType, detail)
+                .userMessage(detail)
+                .fields(problemFields)
+                .build();
+
+        return handleExceptionInternal(ex, problem, headers, status, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
+                                                             HttpStatus status, WebRequest request) {
+
+        if (body == null) {
+            // cairá aqui caso seja alguma exception do spring tratada por
+            // ResponseEntityExceptionHandler (classe extendida)
+            body = Problem.builder()
+                    .title(status.getReasonPhrase())
+                    .status(status.value())
+                    .timestamp(LocalDateTime.now())
+                    .userMessage(MSG_GENERICA_USUARIO_FINAL)
+                    .build();
+        } else if (body instanceof String) {
+            body = Problem.builder()
+                    .title((String) body)
+                    .status(status.value())
+                    .timestamp(LocalDateTime.now())
+                    .userMessage(MSG_GENERICA_USUARIO_FINAL)
+                    .build();
+        }
+        return super.handleExceptionInternal(ex, body, headers, status, request);
+    }
+
+    private Problem.ProblemBuilder createProblemBuilder(HttpStatus status, ProblemType problemType, String detail) {
+        return Problem.builder()
+                .status(status.value())
+                .type(problemType.getUri())
+                .title(problemType.getTitle())
+                .detail(detail)
+                .timestamp(LocalDateTime.now());
+    }
 
 }
